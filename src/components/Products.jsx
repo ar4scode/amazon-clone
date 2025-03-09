132b545adcd27fb9e75fa36d8abfe1ab4bb16b4d
Ar4sCode <ar4sazizi@gmail.com>
2025-03-10 01:12:20 +0330

    move products images into public folder and import all files

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
+import rating from "../assets/ratings/rating-0.png" // for test
 
 const Products = () => {
-  const [products, setProducts] = useState(null)
+  const [products, setProducts] = useState([])
 
   useEffect(() => {
     fetch('../products.json')
@@ -12,8 +13,26 @@ const Products = () => {
 
   
   return (
-    <main>
-      
+    <main className="grid grid-cols-5 gap-4">
+      {products.map((product) => {
+        return (
+          <div className="flex flex-col">
+            <img src={product.image} alt="" />
+            <p>{product.name}</p>
+            <div className="flex">
+              <img src={rating} alt="" />
+              <p>{product.rating.count}</p>
+            </div>
+            <p>{product.priceCents / 100}$</p>
+            <select value="">
+              <option name="one" id="">1</option>
+              <option name="two" id="">2</option>
+              <option name="three" id="">3</option>
+            </select>
+              <button>Add to Cart</button>
+          </div>
+        )
+      })}
     </main>
   )
 }
